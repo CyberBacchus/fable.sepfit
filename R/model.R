805a601a85af4919241bff89a0c03f9c1237226d
Yixin Shi <cyberbacchus@icloud.com>
2024-10-11 17:29:06 +0800

    fit可以，forecast不行，遇到以下问题： 1. 模型无法指定变量 2. mutate出现问题 3. 怎么修改掉construct_fc()

--- R/model.R
+++ R/model.R
@@ -4,48 +4,40 @@
 
 globalVariables("self")
 
-#' @importFrom stats predict
-train_dcmp_sep_fit <- function(.data, specials, ...){
-  # 使用 X13 分解时间序列
-  decomposed <- x13binary::seas(.data)
-  
-  # 获取分解结果中的趋势和季节部分
-  trend <- decomposed$trend
-  seasonal <- decomposed$seasonal
-  
-  # 对趋势部分拟合 ARIMA 模型
+#' 
+train_sep_fit <- function(.data, ...){
+  # 检查数据结构，确保其为tsibble
+  model_data <- .data |> tsibble::as_tsibble()
+
+  # 进行X13分解
+  x13_fit <- model_data |> 
+    fabletools::model(feasts::X_13ARIMA_SEATS()) |>
+    components()
+    
+  # 创建包含趋势和季节性的新数据集
+  decomposed_data <- x13_fit |>
+    dplyr::select(trend, seasonal)
+
+  # 对X13分解后的数据进行建模
   trend_model <- fable::ARIMA(trend)
-  
-  # 对季节部分拟合 SNAIVE 模型
   seasonal_model <- fable::SNAIVE(seasonal)
+
+  # 训练模型
+  trend_fit <- fabletools::model(decomposed_data, trend_model)
+  seasonal_fit <- fabletools::model(decomposed_data, seasonal_model)
   
   # 返回一个包含两个模型的结构体
   structure(
     list(
-      trend_model = trend_model,
-      seasonal_model = seasonal_model,
-      decomposed = decomposed
+      trend_model = trend_fit,
+      seasonal_model = seasonal_fit,
+      decomposed = x13_fit
     ),
-    class = "fbl_dcmp_sep_fit"
+    class = "sep_fit"
   )
 }
 
-specials_dcmp_sep_fit <- new_specials(
-  xreg = function(..., prior_scale = NULL, standardize = "auto", type = NULL){
-    model_formula <- new_formula(
-      lhs = NULL,
-      rhs = reduce(c(0, enexprs(...)), function(.x, .y) call2("+", .x, .y))
-    )
-    list(
-      xreg = model.matrix(model_formula, self$data),
-      prior_scale = prior_scale,
-      standardize = standardize,
-      mode = type
-    )
-  }
-)
-
-#' DcmpSepFit: Decompose time series and fit models to trend and seasonality
+#' sepfit: Decompose time series and fit models to trend and seasonality
 #'
 #' This function decomposes a time series into trend and seasonal components using X13,
 #' and fits an ARIMA model to the trend component and a SNAIVE model to the seasonal component.
@@ -53,45 +45,60 @@ specials_dcmp_sep_fit <- new_specials(
 #' @param formula A symbolic description of the model to be fitted.
 #' @param ... Additional arguments passed to the model fitting process.
 #'
-#' @return A model object of class "dcmp_sep_fit".
+#' @return A model object of class "sep_fit".
 #' @export
-DcmpSepFit <- function(formula, ...){
-  dcmp_sep_fit <- new_model_class("dcmp_sep_fit", train_dcmp_sep_fit, specials = NULL)
-  new_model_definition(dcmp_sep_fit, !!rlang::enquo(formula), ...)
+SepFit <- function(formula, ...){
+  sep_fit <- new_model_class("sep_fit", train_sep_fit, specials = NULL)
+  new_model_definition(sep_fit, !!rlang::enquo(formula), ...)
 }
 
-#' Generate forecasts from a DcmpSepFit model
+#' Forecast method for SepFit models
 #'
-#' @param object A DcmpSepFit model object.
-#' @param new_data New data to generate forecasts for.
-#' @param specials Not used, included for compatibility.
-#' @param ... Additional arguments passed to the forecasting method.
+#' This function generates forecasts for SepFit models by separately forecasting
+#' the trend and seasonal components and then combining them.
 #'
-#' @return A forecasted time series object.
+#' @param object An object of class "sep_fit", typically the result of calling `SepFit()`
+#' @param new_data A tsibble of future time points to forecast
+#' @param specials Special arguments passed to underlying models (unused in this implementation)
+#' @param ... Additional arguments passed to the forecasting process
+#'
+#' @return A fable containing the forecasts
 #' @export
-forecast.fbl_dcmp_sep_fit <- function(object, new_data, specials = NULL, ...){
-  # Combine trend and seasonal model forecasts
+forecast.sep_fit <- function(object, new_data, specials = NULL, ...) {
+  # 提取趋势和季节性模型
   trend_model <- object$trend_model
   seasonal_model <- object$seasonal_model
   
-  trend_forecast <- fable::forecast(trend_model, new_data)
-  seasonal_forecast <- fable::forecast(seasonal_model, new_data)
+  # 对趋势和季节性分别进行预测
+  trend_forecast <- fabletools::forecast(trend_model, new_data = new_data, ...)
+  seasonal_forecast <- fabletools::forecast(seasonal_model, new_data = new_data, ...)
   
-  trend_forecast + seasonal_forecast
+  # 提取预测值
+  trend_values <- trend_forecast$.mean
+  seasonal_values <- seasonal_forecast$.mean
+  
+  # 计算最终预测值（趋势 * 季节性）
+  final_forecast <- trend_values * seasonal_values
+  
+  # 创建分布对象
+  forecast_dist <- distributional::dist_degenerate(final_forecast)
+  
+  # 构建fable对象
+  fabletools::construct_fc(forecast_dist, new_data)
 }
 
-#' Obtain fitted values from a DcmpSepFit model
+#' Obtain fitted values from a sepfit model
 #'
-#' @param object A DcmpSepFit model object.
+#' @param object A sepfit model object.
 #' @param ... Additional arguments passed to the fitted method.
 #'
 #' @return A vector of fitted values.
 #' @export
-fitted.fbl_dcmp_sep_fit <- function(object, ...){
+fitted.sep_fit <- function(object, ...){
   trend_model <- object$trend_model
   seasonal_model <- object$seasonal_model
   
-  fitted_values <- fable::fitted(trend_model) + fable::fitted(seasonal_model)
+  fitted_values <- fable::fitted(trend_model) * fable::fitted(seasonal_model)
   fitted_values
 }
 
@@ -105,11 +112,11 @@ mean.dist_symmetric_percentile <- function(x, ...){
 }
 
 #' @export
-model_sum.fbl_dcmp_sep_fit <- function(x){
-  "DcmpSepFit Model"
+model_sum.sep_fit <- function(x){
+  "sepfit Model"
 }
 
 #' @export
-format.fbl_dcmp_sep_fit <- function(x, ...){
-  "DcmpSepFit Model"
+format.sep_fit <- function(x, ...){
+  "sepfit Model"
 }
